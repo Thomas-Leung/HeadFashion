61b8aca33a14d24bec503f129e4ad0f341126d4c
Thomas <32366586+Thomas-Leung@users.noreply.github.com>
2024-10-31 15:06:04 -0400

    fix image ratio and update icon

--- scripts/content.js
+++ scripts/content.js
@@ -148,18 +148,18 @@ for (let resizer of resizers) {
 
       if (currentResizer.classList.contains("se")) {
         el.style.width = rect.width - (prevX - e.clientX) + "px";
-        el.style.height = rect.height - (prevY - e.clientY) + "px";
+        el.style.height = rect.width - (prevY - e.clientY) + "px";
       } else if (currentResizer.classList.contains("sw")) {
         el.style.width = rect.width + (prevX - e.clientX) + "px";
-        el.style.height = rect.height - (prevY - e.clientY) + "px";
+        el.style.height = rect.width - (prevY - e.clientY) + "px";
         el.style.left = rect.left - (prevX - e.clientX) + "px";
       } else if (currentResizer.classList.contains("ne")) {
         el.style.width = rect.width - (prevX - e.clientX) + "px";
-        el.style.height = rect.height + (prevY - e.clientY) + "px";
+        el.style.height = rect.width + (prevY - e.clientY) + "px";
         el.style.top = rect.top - (prevY - e.clientY) + "px";
       } else {
         el.style.width = rect.width + (prevX - e.clientX) + "px";
-        el.style.height = rect.height + (prevY - e.clientY) + "px";
+        el.style.height = rect.width + (prevY - e.clientY) + "px";
         el.style.top = rect.top - (prevY - e.clientY) + "px";
         el.style.left = rect.left - (prevX - e.clientX) + "px";
       }
